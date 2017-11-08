40e3708739a97c52133bcd71a7ffebb401a1c204
Cassandra Wilcox <cassie@codehangar.io>
2017-11-07 20:12:44 -0500

    add and send email templates

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   // Loads environment settings from '.env' into process.env
   // This is for local development
-  require('dotenv').config();
+  require('dotenv')
+    .config();
 }
 import express from 'express';
 import nodemailer from 'nodemailer';
+import Email from 'email-templates';
 
 const app = express();
-
-app.use(express.static('public'));
-
-app.get('/', (req,res) => {
-  res.sendfile('index.html');
-});
 
 const smtpTransport = nodemailer.createTransport({
   service: "gmail",
@@ -24,71 +20,53 @@ const smtpTransport = nodemailer.createTransport({
   }
 });
 
-app.get('/send',function(req,res){
+const email = new Email({
+  transport: smtpTransport,
+  send: true
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendfile('index.html');
+});
+
+app.get('/send', function (req, res) {
+
+  const context = {
+    name: req.query.name,
+    email: req.query.email,
+    message: req.query.message,
+    fromName: 'Cassandra Wilcox',
+    fromEmail: 'cassandra@cassandrawilcox.me'
+  };
 
   const leadMailOptions = {
-    to: req.query.email,
-    subject: "Thanks for reaching out!",
-    text: `Hey there ${req.query.name}!
-    
-    Thank you so much for emailing me. I will be in touch with your shortly. 
-    
-    For your records, a copy of the message I received from you is below:
-    
-    -----
-    
-    Your name: 
-    ${req.query.name}
-    
-    Your email: 
-    ${req.query.email}
-    
-    Your Message:
-    ${req.query.message}
-    
-    -----
-    
-    Kind regards,
-    
-    Cassandra Wilcox
-    cassandra@cassandrawilcox.me
-    `
+    to: req.query.email
   };
 
   const selfMailOptions = {
-    to: "cassie@codehangar.io",
-    subject: "New contact form submission from " + req.query.name,
-    text: `You have a new email from ${req.query.name}!
-    
-    Their email is ${req.query.email}.
-    
-    Here's what they have to say:
-    ${req.query.message}
-    `
+    to: 'cassandra@cassandrawilcox.me'
   };
 
-  // console.log(leadMailOptions);
-  // console.log(selfMailOptions);
-  smtpTransport.sendMail(leadMailOptions, function(error, response){
-    if(error){
-      console.log(error);
-      res.end("error");
-    }else{
-      console.log("Message sent: " + response.message);
-      res.end("sent");
-    }
-  });
-  smtpTransport.sendMail(selfMailOptions, function(error, response){
-    if(error){
-      console.log(error);
-      res.end("error");
-    }else{
-      console.log("Message sent: " + response.message);
-      res.end("sent");
-    }
-  });
+  email.send({
+    template: 'lead-confirmation',
+    message: leadMailOptions,
+    locals: context
+  })
+       .then(res.end("sent"))
+       .catch(res.end("error"));
+
+  email.send({
+    template: 'new-contact',
+    message: selfMailOptions,
+    locals: context
+  })
+       .then(res.end("sent"))
+       .catch(res.end("error"));
+
 });
 
 app.listen(3000, () => {
   console.log("Express started on port 3000...");
-})
+});
